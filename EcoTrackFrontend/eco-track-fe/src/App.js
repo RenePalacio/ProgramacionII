42966222c0910d1f6dae4847ae84ab6ad17b72c9
rp1065012023@unab.edu.sv <rp1065012023@unab.edu.sv>
2024-09-27 21:44:33 -0600

    FrontEnd

--- EcoTrackFrontend/eco-track-fe/src/App.js
+++ EcoTrackFrontend/eco-track-fe/src/App.js
@@ -1,6 +1,6 @@
 import './styles.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home'; // Componente de la primera pantalla
+
 import Dashboard from './Dashboard';
 import Register from './Register'; // Componente de la segunda pantalla (Inicia sesión)
 
@@ -9,10 +9,10 @@ function App() {
     <Router>
       <Routes>
         {/* Ruta principal (Pantalla de inicio) */}
-        <Route path="/" element={<Home />} />
+        
 
         {/* Ruta del Dashboard (Segunda pantalla después de iniciar sesión) */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
 
         <Route path="/register" element={<Register />} />
       </Routes>
--- EcoTrackFrontend/eco-track-fe/src/Home.js
+++ EcoTrackFrontend/eco-track-fe/src/Home.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Home.css';
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import './styles.css';
 
-function Home() {
-  const navigate = useNavigate(); // Hook para redirigir
+// function Home() {
+//   const navigate = useNavigate(); // Hook para redirigir
 
-  const handleLogin = () => {
-    navigate('/dashboard'); // Redirige al dashboard
-  };
+//   const handleLogin = () => {
+//     navigate('/dashboard'); // Redirige al dashboard
+//   };
 
-  const handleRegister = () => {
-    navigate('/register'); // Redirige a la página de registro
-  };
+//   const handleRegister = () => {
+//     navigate('/register'); // Redirige a la página de registro
+//   };
 
-  return (
-    <div className="home-container">
-      <div className="welcome-section">
-        <h1 className="welcome-title">¡BIENVENIDO!</h1>
-        <p className="welcome-message">Hola, qué gusto tenerte por aquí en EcoTrack. Da el salto a tener un horario interactivo y amigable </p>
-      </div>
+//   return (
+//     <div className="home-container">
+//       <div className="welcome-section">
+//         <h1 className="welcome-title">¡BIENVENIDO!</h1>
+//         <p className="welcome-message">Hola, qué gusto tenerte por aquí en EcoTrack. Da el salto a tener un horario interactivo y amigable</p>
+//       </div>
 
-      <div className="button-container">
-        <button className="sign-in-button" onClick={handleLogin}>Inicia Sesión</button>
-        <button className="register-button" onClick={handleRegister}>Regístrate aquí</button>
-      </div>
+//       <div className="button-container">
+//         <button className="sign-in-button" onClick={handleLogin}>Inicia Sesión</button>
+//         <button className="register-button" onClick={handleRegister}>Regístrate aquí</button>
+//       </div>
 
-      {/* Elementos decorativos */}
-      <div className="ground"></div>
-      <div className="ocean"></div>
-    </div>
-  );
-}
+//       {/* Sección de pasto en movimiento */}
+//       <div className="ground"></div>
+//     </div>
+//   );
+// }
 
-export default Home;
+// export default Home;
